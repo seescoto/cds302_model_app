d28cfe81dbe4c5d13984084c358813a10624ecdd
seescoto <sescoto@gmu.edu>
2021-11-17 00:28:54 -0500

    begginnings of app (not pretty yet!!)

--- app.R
+++ app.R
@@ -1,14 +1,50 @@
 library(shiny)
 
 
+#user interface so they can interact with stuff
 ui <- pageWithSidebar(
   
   # App title ----
-  headerPanel("Will you get approved for a loan?"),
+  titlePanel(" Will you get approved for a loan?"),
   
-  # Sidebar panel for inputs ----
-  sidebarPanel(),
+  
+  
+  #  inputs ---- should be loan percent rate and loan_percent_income
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(inputId = 'percent_int', #what we enter into predict()
+                    label = "What is the highest percent interest you're willing to pay?",
+                    min = 0,
+                    max = 36, #max
+                    value = 10 #avg interest rate
+                    ),
+        
+        #income  input
+        textInput(inputId = 'income',
+                  label = "What is your yearly income?",
+                  value = '50000'),
+        
+        #loan amount input
+        textInput(inputId = 'loan',
+                  label = "How much money would you like to borrow?",
+                  value = '10000')
+        
+                   ),
+      
+                  
+      
+ 
+  
   
   # Main panel for displaying outputs ----
-  mainPanel()
+      mainPanel()
+                )
 )
+
+#predict function based on our lrm
+server <- function(input, output, session) {
+  
+}
+
+
+shinyApp(ui, server)
