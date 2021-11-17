6462714f34081cb640b2d1241f7f0d6dfbf9b21e
seescoto <sescoto@gmu.edu>
2021-11-17 00:58:35 -0500

    :)

--- app.R
+++ app.R
@@ -2,48 +2,59 @@ library(shiny)
 
 
 #user interface so they can interact with stuff
-ui <- pageWithSidebar(
+ui <- fluidPage(
   
   # App title ----
   titlePanel(" Will you get approved for a loan?"),
   
-  
-  
-  #  inputs ---- should be loan percent rate and loan_percent_income
+  #  inputs in sidebar ---- should be loan percent rate and loan_percent_income
     sidebarLayout(
       sidebarPanel(
-        sliderInput(inputId = 'percent_int', #what we enter into predict()
-                    label = "What is the highest percent interest you're willing to pay?",
-                    min = 0,
-                    max = 36, #max
-                    value = 10 #avg interest rate
+                 sliderInput(inputId = 'percent_int', #what we enter into predict()
+                             label = "What is the highest percent interest you're willing to pay?",
+                             min = 0, max = 36, value = 10 #avg interest rate
                     ),
-        
-        #income  input
-        textInput(inputId = 'income',
-                  label = "What is your yearly income?",
-                  value = '50000'),
-        
-        #loan amount input
-        textInput(inputId = 'loan',
-                  label = "How much money would you like to borrow?",
-                  value = '10000')
-        
-                   ),
       
-                  
+         
+                #income  input
+                textInput(inputId = 'income',
+                          label = "What is your yearly income?",
+                          value = '50000'),
       
- 
-  
+                
+                #loan amount input
+                textInput(inputId = 'loan',
+                          label = "How much money would you like to borrow?",
+                          value = '10000')),
+        
+
   
   # Main panel for displaying outputs ----
-      mainPanel()
-                )
-)
+      mainPanel(
+        # Output: Formatted text for caption ----
+        #h3(textOutput("caption")),
+        
+        # Output: Plot of the requested variable against mpg ----
+        #plotOutput("mpgPlot")
+        
+      ),
+  position = c('left', 'right'),
+  fluid = TRUE
+  
+  
+  )) 
+
+
 
 #predict function based on our lrm
 server <- function(input, output, session) {
   
+  dfinput <- reactive({
+    switch(input$dataset,
+           "percent_int" = percent_int,
+           "loan_percent" = loan/income)
+  
+  })
 }
 
 
