9145c345a3fbb79f8faaea95020fb58ef634b527
seescoto <sescoto@gmu.edu>
2021-11-17 14:32:27 -0500

    idk 2.0

--- workout.R
+++ workout.R
@@ -27,3 +27,30 @@ d
 
 source_python('python_model.py')
 
+
+
+
+library(tidyverse)
+library(modelr)
+d <- read_csv('https://raw.githubusercontent.com/seescoto/cds302_model_app/main/credit_risk_dataset.csv')
+
+mod <- glm(loan_status ~ loan_percent_income + loan_int_rate, data = d, 
+           family = 'binomial')
+
+summary(mod)
+
+d %>% 
+  ggplot() +
+  geom_point(aes(loan_percent_income, loan_int_rate, color = loan_status))
+
+mod2 <- glm(loan_status ~ loan_percent_income * loan_int_rate, data = d, 
+            family = 'binomial')
+
+summary(mod2)
+
+
+
+new <- data_frame(loan_percent_income = .59, 
+                 loan_int_rate = 11.02) 
+
+add_predictions(mod, type = 'response', data = new)
