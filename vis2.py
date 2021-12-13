dfa65f79364b9b800928157be2e7f5ff34b2e6f7
Davis2020 <55894495+Davis2020@users.noreply.github.com>
2021-12-13 01:03:57 -0500

    Update vis2.py

--- vis2.py
+++ vis2.py
@@ -18,7 +18,7 @@ def app():
 
     for percent_complete in range(100):
         time.sleep(0.1)
-        my_bar.progress(percent_complete + 1)
+        my_bar.progress(percent_complete + 0.5)
     
     
     
