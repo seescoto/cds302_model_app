2c5d9098ab642a0770d48c8b5e23429838d8627e
Davis2020 <55894495+Davis2020@users.noreply.github.com>
2021-12-12 23:49:26 -0500

    Update app3.py

--- app3.py
+++ app3.py
@@ -84,24 +84,3 @@ def app(): #create random test values
         st.pyplot(fig)
         
         
-    st.header("For bugs encounters please contact us!")
-    contact_form = """
-    <form action="https://formsubmit.co/davis1kajuna@gmail.com" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here."></textarea>
-     <button type="submit">Send</button>
-    </form>
-    """
-    
-    st.markdown(contact_form, unsafe_allow_html=True)
-    
-    # Use Local CSS File
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-    css_path = "style/style.css"
-    local_css(css_path)
