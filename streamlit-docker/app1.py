711009d2bc1124a09c2c3aaaf815962436974d66
jmwyrick <94394148+jmwyrick@users.noreply.github.com>
2021-12-13 13:56:38 -0500

    Updated Docker container to line up with main code

--- streamlit-docker/app1.py
+++ streamlit-docker/app1.py
@@ -1,62 +1,127 @@
-#app 1 - group guessing given a csv
+def app(): # group csv test
+    import streamlit as st
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegression
+    from sklearn import metrics
+    import matplotlib.pyplot as plt
+    import numpy as np
+    #app 1 - group guessing given a csv
 
-import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-import numpy as np
-
-#title and stuff idk
-st.title('Credit Loan Risk')
-st.subheader('Will you get approved for a loan?')
-
-
-# data and model loading
-
-#loading data
-path = 'https://raw.githubusercontent.com/seescoto/cds303_model_app/main/updated_csv.csv'
-credit_risk = pd.read_csv(path)
-Feature_cols = ['interest_rate', 'loan_percent_income']
-
-#fitting the model
-#setting x and y
-X = credit_risk[Feature_cols]
-y = credit_risk.loan_status
-#split into test/train sets - no randomness so same results each time
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20, random_state = 0)
-#creating model and fitting it
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
-
-#model and data fitted!
+    #title and stuff idk
+    st.title('Credit Loan Risk')
+    st.subheader('Enter a CSV file to determine loan approval')
 
 
-#describe interest rate stuff for looking at it idk
-#st.text(credit_risk['interest_rate'].describe())
+    # data and model loading
+
+    #loading data
+    path = 'https://raw.githubusercontent.com/seescoto/cds303_model_app/main/updated_csv.csv'
+    credit_risk = pd.read_csv(path)
+    Feature_cols = ['interest_rate', 'loan_percent_income']
+
+    #fitting the model
+    #setting x and y
+    X = credit_risk[Feature_cols]
+    y = credit_risk.loan_status
+    #split into test/train sets - no randomness so same results each time
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20, random_state = 0)
+    #creating model and fitting it
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    #y_pred = logreg.predict(X_test)
+
+    #model and data fitted!
 
 
-###use inputs in sidebar
+    #describe interest rate stuff for looking at it idk
+    #st.text(credit_risk['interest_rate'].describe())
 
 
-dat = st.sidebar.file_uploader('Please upload the data in CSV form', type = 'csv')
-caps = ['Your CSV file should have two columns.',
-"The first one should represent the interest rate for the given loan, and the second should represent what percentage of said person's income is their requested loan.",
-'E.G. if they make $50,000 and want a loan of $10,000, their loan percent income would be 10,000/50,000 = 1/5 = 0.2',
-'example row for a loan that is 20% of the income with a 15% interest rate: .2, 15']
-
-for i in caps:
-    st.sidebar.caption(i)
-
-if dat != None:
-    dat = pd.read_csv(dat, sep = ',')
-
-    st.write(dat)
-
-    ##modeling and getting the answer after checkbox has been selected
-    dat.pred = logreg.predict(dat) #predict defaulting [1] 0 is approved/not default
-    dat.prob = logreg.predict_proba(dat) #predict possibility of not defaulting and not defaulting
+    ###use inputs in sidebar
 
 
-    st.write(dat)
+    dat = st.file_uploader('Please upload the data in CSV form', type = 'csv')
+    caps = ['Your CSV file should have two columns.',
+    "The first one should represent the interest rate for the given loan, and the second should represent what percentage of said person's income is their requested loan.",
+    'E.G. if they make $50,000 and want a loan of $10,000, their loan percent income would be 10,000/50,000 = 1/5 = 0.2',
+    'example row for a loan that is 20% of the income with a 15% interest rate: 15, .2']
+
+    for i in caps:
+        st.caption(i)
+
+    if dat != None:
+        dat = pd.read_csv(dat, sep = ',')
+
+        if len(dat.columns) != 2:
+            st.warning('Please upload a CSV file with two columns')
+        else: #go on regularly
+            cols = ['interest_rate', 'loan_percent_income']
+            new = []
+            i = -1
+            for c in dat.columns:
+                i += 1
+                if c != cols[i]: #if column names aren't correct
+                    dat.columns = cols #replace first pair w/ col names
+                    try: #if the column names are numbers, add to new
+                        new.append(float(c))
+                    except: #if not numbers, just go to next thing
+                        break
+
+            if new: #if we added to new, make it a df combine it with dat
+                new = pd.DataFrame([new], columns = cols)
+                dat = pd.concat([new, dat])
+
+
+            st.text('Calculating...')
+
+
+            loans = logreg.predict(dat)
+            probs = logreg.predict_proba(dat)[:, 0]
+            dat['loan_approved'] = loans
+            dat['prob_of_approval'] = probs
+
+            repdict = {1 : 'Denied', 0: 'Approved'}
+
+
+            dat['loan_approved'] = dat['loan_approved'].map(repdict)
+
+            st.write(dat)
+
+            #put in a plot of values approved or not
+
+
+            fig = plt.figure()
+
+            groups = dat.groupby("loan_approved")
+            for name, group in groups:
+                plt.plot(group["interest_rate"], group["loan_percent_income"] * 100,
+                marker = 'o', linestyle=" ", label=name)
+            plt.legend()
+            plt.title('Loan Applications Approved/Denied')
+            plt.xlabel('Interest rate for loan')
+            plt.ylabel('Loan is ___ percent of income')
+
+            st.pyplot(fig)
+            
+    st.header("For bugs encounters please contact us!")
+    contact_form = """
+    <form action="https://formsubmit.co/davis1kajuna@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here."></textarea>
+     <button type="submit">Send</button>
+    </form>
+    """
+    
+    st.markdown(contact_form, unsafe_allow_html=True)
+    
+    # Use Local CSS File
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+    css_path = "style/style.css"
+    local_css(css_path)
--- streamlit-docker/st_app.py
+++ streamlit-docker/st_app.py
@@ -1,4 +1,7 @@
-###streamlit app - sofia escoto
+#MULTIPLE PAGES
+#FROM https://medium.com/@u.praneel.nihar/
+#building-multi-page-web-app-using-streamlit-7a40d55fa5b4
+#!pip install seaborn
 
 import streamlit as st
 import pandas as pd
@@ -6,110 +9,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import numpy as np
+import csv
+import seaborn as sns
 
 
-#title and stuff idk
-st.title('Credit Loan Risk')
-st.subheader('Will you get approved for a loan?')
+#cheat sheet for adding the visualization page
+#make your python file and then name it something like vis.py
+#(or change it and change code)
+#put your entire code inside a function defining app
+#so like def app():
+#then an indent and ur entire code for the page
+#and import vis and then uncomment the visualizations part of the dict
 
 
-# data and model loading
+import app1
+import app2
+import app3
+import vis
+import vis2
+import streamlit as st
+PAGES = {
+    "Evaluate 1 loan application at a time": app2,
+    "Upload CSV of applications": app1,
+    "Evaluate random applications": app3,
+    "Model Visualizations" : vis,
+    "Data Exploaratory Analysis" : vis2
 
-#loading data
-path = 'https://raw.githubusercontent.com/seescoto/cds303_model_app/main/credit_risk_dataset.csv'
-df = pd.read_csv(path)
-#drop categorical data
-credit_risk = df.select_dtypes(exclude=['object'])
-#renaming columns
-#Renaming columns
-credit_risk.columns = ['age', 'income', 'employment_length', 'amount', 'interest_rate',
- 'loan_status', 'loan_percent_income', 'credit_length']
-#mean imputation
-credit_risk["interest_rate"].fillna(credit_risk["interest_rate"].mean(), inplace = True)
-#getting important cols for model (based on what correlates with loan status)
-Feature_cols = ['interest_rate', 'loan_percent_income']
-
-#fitting the model
-#setting x and y
-X = credit_risk[Feature_cols]
-y = credit_risk.loan_status
-#split into test/train sets - no randomness so same results each time
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20, random_state = 0)
-#creating model and fitting it
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
-
-#model and data fitted!
-
-
-#describe interest rate stuff for looking at it idk
-#st.text(credit_risk['interest_rate'].describe())
-
-
-###use inputs in sidebar
-
-#loan amount
-loan_amt = st.sidebar.number_input('How much money would you like to loan in dollars?', value = 10000)
-if loan_amt <= 0:
-    st.warning('Loan amount must be a positive non-zero value.')
-
-#income
-income = st.sidebar.number_input('What is your annual income in dollars?', value = 50000)
-if income < 0:
-    st.warning('Income can not be a negative value.')
-
-#loan percent interest calculated from above
-loan_percent_test = loan_amt/(income + 0.01) #so no dividing by 0 even if income is 0
-
-#interest rate
-opts = ['Yes, I know my interest rate',
-'No, please estimate for me']
-
-ints = st.sidebar.selectbox(
-'Do you know what interest rate you would be approved for if given a loan?', opts)
-
-if ints == opts[0]: #if they know their interest rate
-    int_test = st.sidebar.number_input('Please enter the interest rate you were previously approved for:',
-    value = 12.00)
-elif ints == opts[1]: #if they dont know
-    #credit score to estimate interest rate
-    #the bank gives an interest rate and a random person might not know what theirs would be
-    #so we'll just give them one based off an educated guess
-    credit_test = st.sidebar.number_input('What is your current credit score?',
-    value = 600)
-    if credit_test < 0 or credit_test > 850:
-        st.warning('Credit score must be an integer between 0 and 850.')
-    int_rates = (list(np.linspace(20.58, 17.12, 500 - 300 + 1)) +
-    list(np.linspace(17.11, 10.5, 600 - 501 + 1)) +
-    list(np.linspace(10.49, 5.5, 660 - 601 + 1)) +
-    list(np.linspace(5.49, 3.67, 780 - 661 + 1)) +
-    list(np.linspace(3.66, 3.66, 850 - 781 + 1)))
-                #avg interest rates based on credit score
-                #https://www.businessinsider.com/personal-finance/average-auto-loan-interest-rate
-                #closest to credit_risk['interest_rate'].describe() stats
-    #851 values for interest, one for each credit score 300 to 850
-    #input for model - int_test
-    if credit_test < 300:
-        #if less than 300 credit score, max interest
-        int_test = 36.0
-    else:
-        int_test = int_rates[credit_test - 300]
-    st.sidebar.caption(f'Based on your credit score, your interest rate should be around {round(int_test, 2)}%')
-    st.sidebar.caption('Estimations of interest rate based on credit score may not yield accurate results.')
-else:
-    st.warning(f'Please enter either your interest rate or credit score')
-
-
-
-
-##modeling and getting the answer after checkbox has been selected
-input = [[int_test, loan_percent_test]]
-pred = logreg.predict(input) #predict defaulting [1] - aka NOT paying. 0 is approved/not default
-prob = logreg.predict_proba(input) #predict possibility of not defaulting and not defaulting
-if pred == 0:
-    st.text(f'Our model predicts you will get approved for a loan!')
-    st.text(f'The probability that you will be approved is {round(prob[0][0]*100, 2)}%')
-else:
-    st.text(f'Our model predicts that you will not get approved for a loan.')
-    st.text(f'The probability that you will be approved is {round(prob[0][0]*100, 2)}%')
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
